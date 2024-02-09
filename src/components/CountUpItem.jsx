e02103fde2c331e830ec27ee2ee4ead6c5abe46e
Carlos Morales <cmja.27@gmail.com>
2024-02-09 19:44:26 -0400

    feature: Counter improvement

--- src/components/CountUpItem.jsx
+++ src/components/CountUpItem.jsx
@@ -1,16 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import CountUp from 'react-countup';
 
 const CountUpItem = ({ end, duration, label, counterOn }) => {
     return (
       <>
-        {counterOn && (
-          <CountUp start={0} end={end} duration={duration}>
-            {({ countUpRef }) => (
-              <h3 className="stats__name" ref={countUpRef}></h3>
-            )}
-          </CountUp>
-        )}
+        {counterOn && <CountUp  className="stats__name" start={0} end={end} duration={duration}/>}
         <h4 className="stats__description">{label}</h4>
       </>
     );
